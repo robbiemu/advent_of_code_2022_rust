0903c12f4a0eea9b3c95e153f1ee39690523c98b
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2023-07-28 09:17:20 -0400

    wip
    regex
    
    
    .

--- day_21/src/part2.rs
+++ day_21/src/part2.rs
@@ -3,7 +3,7 @@ use part2_solver::ProblemSolverPattern;
 mod problem_solver;
 use problem_solver::solve_problem;
 pub mod common;
-mod ru_symbols_simple_parser;
+mod simple_parser;
 
 fn main() {
   solve_problem::<ProblemSolverPattern>();
